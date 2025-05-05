4eaf40d01d39ecf515a788e1bd1030b7c3836a80
Jared Stoker <stokerfam@gmail.com>
2025-05-05 12:35:00 -0600

    Update commands.ts

--- src/utils/commands.ts
+++ src/utils/commands.ts
@@ -9,7 +9,7 @@ const hostname = 'www.jaredstoker.com'
 
 export const commands: Record<string, (args: string[]) => Promise<string> | string> = {
   help: () => 'Available commands: \n' + Object.keys(commands).join('\n '),
-  hostname: () => "www.jaredstoker.com",
+  hostname: () => hostname,
   whoami: () => 'guest',
   date: () => new Date().toLocaleString(),
   //vi: () => `why use vi? try 'emacs'`,
